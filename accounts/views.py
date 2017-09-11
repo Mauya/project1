680017051f77c091136970c3d6eee3db5e7cdfc7
Mauya <smkadungure@live.co.uk>
2017-09-11 01:11:36 +0100

    Initial commit

---
+++ accounts/views.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, redirect
+from accounts.forms import RegistrationForm
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserChangeForm
+
+def home(request):
+    name = 'Sandra Kadungure'
+
+    args = {'myName': name}
+    return render(request, 'accounts/home.html', args)
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/account')
+    else:
+        form = RegistrationForm()
+
+        args = {'form': form}
+    return render(request, 'accounts/reg_form.html', args)
+
+def view_profile(request):
+    args = {'user': request.user}
+    return render(request, 'accounts/profile.html', args)
+
+def edit_profile(request):
+    if request.method == 'POST':
+        form = UserChangeForm(request.POST, instance=request.user)
+
+        if form.is_valid():
+            form.save()
+            return redirect('/accounts/profile')
+    else:
+        form = UserChangeForm(instance=request.user)
+        args = {'form': form}
+    return render(request, 'accounts/edit_profile.html', args)
